c3d469e23f04f6f8abe26365e29f1ccb4ab48f8f
fatimagulomova <fatimakhongulomova@gmail.com>
2025-07-03 13:57:49 +0500

    Adding Error handling

--- scripts/load_script.py
+++ scripts/load_script.py
@@ -24,15 +24,31 @@ client = MongoClient(
 db = client["sensor_data"]
 collection = db["readings"]
 
-df = pd.read_csv("data/sample_cleaned_sensors.csv")
 
-# Batch insert
-total = len(df)
-for i in range(0, total, BATCH_SIZE):
-    batch = df[i:i+BATCH_SIZE]
-    collection.insert_many(batch.to_dict(orient="records"))
-    print(f"Inserted batch {i//BATCH_SIZE + 1}: {len(batch)} records.")
-    time.sleep(1)
+try: 
+    df = pd.read_csv("data/sample_cleaned_sensors.csv")
+
+    # Batch insert
+    total = len(df)
+
+    for i in range(0, total, BATCH_SIZE):
+        batch = df[i:i+BATCH_SIZE]
+        collection.insert_many(batch.to_dict(orient="records"))
+        print(f"Inserted batch {i//BATCH_SIZE + 1}: {len(batch)} records.")
+        time.sleep(1)
+
+except FileNotFoundError as err:
+    print("No file")
+    df = pd.read_csv("data/iot_telemetry_data.csv")
+    sample_data = df.dropna().sample(frac=0.1, random_state=42)
+    
+    total = len(sample_data)
+
+    for i in range(0, total, BATCH_SIZE):
+        batch = sample_data[i:i+BATCH_SIZE]
+        collection.insert_many(batch.to_dict(orient="records"))
+        print(f"Inserted batch {i//BATCH_SIZE + 1}: {len(batch)} records.")
+        time.sleep(1)
 
 print(f"Finished inserting {total} records in batches of {BATCH_SIZE}.")
 
